f08cddb84c51a3d670c3d74d6fe63a8d07bdbc97
Patrick Rideout <prideout2002@gmail.com>
2023-12-05 15:41:29 -0330

    Fixed edit button issue.

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,31 +6,52 @@ router.get('/new', (req, res) => {
     res.render('recipes/new', { recipe: new Recipe() });
 });
 
-router.get('/:slug', async (req, res) => {
-    const recipe = await Recipe.findOne({slug: req.params.slug})
-    if (recipe == null) res.redirect('/')
-    res.render('recipes/show', {recipe: recipe})
-})
+router.get('/edit/:id', async (req, res) => {
+    const recipe =  await Recipe.findById(req.params.id)
+    res.render('recipes/edit', { recipe: recipe });
+});
 
-router.post('/', async (req, res) => {
-    let recipe = new Recipe({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown,
-    })
+
+router.get('/:slug', async (req, res) => {
     try {
-        recipe = await recipe.save()
-        res.redirect(`/recipes/${recipe.slug}`)
+        const recipe = await Recipe.findOne({slug: req.params.slug})
+        if (recipe == null) res.redirect('/')
+        res.render('recipes/show', {recipe: recipe})
     } catch(e) {
-        res.render('recipes/new', {recipe: recipe})
         console.log(e)
     }
 })
+
+router.post('/', async (req, res, next) => {
+    req.recipe = new Recipe()
+    next()
+}, saveRecipeAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.recipe = await Recipe.findById(req.params.id)
+    next()
+}, saveRecipeAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Recipe.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveRecipeAndRedirect(path) {
+    return async (req, res) => {
+      let recipe = req.recipe
+      recipe.title = req.body.title
+      recipe.description = req.body.description
+      recipe.markdown = req.body.markdown
+      try {
+        recipe = await recipe.save()
+        res.redirect(`/recipes/${recipe.slug}`)
+      } catch (e) {
+        res.render(`recipes/${path}`, { recipe: recipe })
+      }
+    }
+  }
+  
   
 
 module.exports = router;
