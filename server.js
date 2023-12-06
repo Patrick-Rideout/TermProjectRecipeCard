915f214e04c7f8e80a09ed554dbcffd9c466b7bc
Patrick Rideout <prideout2002@gmail.com>
2023-12-06 01:43:45 -0330

    Added my own online database using mongoDB Atlas.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Article = require('./models/recipe')
-const articleRouter = require('./routes/recipes')
+const Recipe = require('./models/recipe')
+const recipeRouter = require('./routes/recipes')
 const methodOverride = require('method-override')
 const app = express();
  
-mongoose.connect('mongodb://localhost/blog')
+mongoose.connect('mongodb+srv://RecipeCardAdmin:123CNA123@recipecardcluster.qigg8sx.mongodb.net/')
 
 app.set('view engine', 'ejs');
 
@@ -15,10 +15,10 @@ app.use(methodOverride('_method'))
 
 
 app.get('/', async (req, res) => {
-    const recipes = await Article.find().sort({ title: 'asc'})
+    const recipes = await Recipe.find().sort({ title: 'asc'})
     res.render('recipes/index', { recipes : recipes});
 })
 
-app.use('/recipes', articleRouter);
+app.use('/recipes', recipeRouter);
 
 app.listen(4000);
