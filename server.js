20e97118351ec0ad8d09c2d17cbba5298ce5c362
Patrick Rideout <prideout2002@gmail.com>
2023-12-07 01:23:26 -0330

    Working search function.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Recipe = require('./models/recipe')
 const recipeRouter = require('./routes/recipes')
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const { appendFile } = require('fs/promises');
 const app = express();
  
 mongoose.connect('mongodb+srv://RecipeCardAdmin:123CNA123@recipecardcluster.qigg8sx.mongodb.net/')
@@ -15,9 +16,38 @@ app.use(methodOverride('_method'))
 
 
 app.get('/', async (req, res) => {
+    
     const recipes = await Recipe.find().sort({ title: 'asc'})
+    
     res.render('recipes/index', { recipes : recipes});
 })
+
+app.get('/search', async (req, res) => {
+    
+    
+    const { difficulty, category } = req.query;
+    console.log(difficulty);
+    console.log(category);
+
+    let query = {};
+    if (difficulty !== 'Any' && category !== 'Any') {
+        query = req.query
+    }
+    else {
+        if (difficulty !== 'Any') {
+            query.difficulty = difficulty;
+
+        }
+        if (category !== 'Any') {
+            query.category = category;
+        }
+    }
+    console.log(query);
+
+    const recipes = await Recipe.find(query).sort({ title: 'asc' });
+    res.render('recipes/index', { recipes : recipes});
+});
+
 
 app.use('/recipes', recipeRouter);
 
